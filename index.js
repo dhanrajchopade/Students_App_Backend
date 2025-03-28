9f6719c74f733692a48aa72058f1295ac1b1846a
dhanrajchopade <dhanraj.chopade7@gmail.com>
2025-03-28 18:47:09 +0530

    removed seeding fucntion

--- index.js
+++ index.js
@@ -12,34 +12,34 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
-initializeDatabase()
+// initializeDatabase()
 
-const jsonData = fs.readFileSync("studentData.json","utf-8")
-const StudentData = JSON.parse(jsonData)
+// const jsonData = fs.readFileSync("studentData.json","utf-8")
+// const StudentData = JSON.parse(jsonData)
 
-async function seedData() {
-    try{
-        for(const studentData of StudentData){
-            const newStudent = new Student({
-name:studentData.name,
-gender: studentData.gender,
-profilepicUrl: studentData.profilepicUrl,
-previousYearMarks: studentData.previousYearMarks,
-details: studentData.details,
-dressCode: studentData.dressCode,
-skills: studentData.skills,
-feesPending: studentData.feesPending,
-parentsDetails: studentData.parentsDetails,
-             })
-            await newStudent.save()
-            console.log("Data Seeding completed successfully.")
-        }
-    }catch(error){
-        console.log("An error occured while seeding the data.", error)
-    }
+// async function seedData() {
+//     try{
+//         for(const studentData of StudentData){
+//             const newStudent = new Student({
+// name:studentData.name,
+// gender: studentData.gender,
+// profilepicUrl: studentData.profilepicUrl,
+// previousYearMarks: studentData.previousYearMarks,
+// details: studentData.details,
+// dressCode: studentData.dressCode,
+// skills: studentData.skills,
+// feesPending: studentData.feesPending,
+// parentsDetails: studentData.parentsDetails,
+//              })
+//             await newStudent.save()
+//             console.log("Data Seeding completed successfully.")
+//         }
+//     }catch(error){
+//         console.log("An error occured while seeding the data.", error)
+//     }
     
-}
-seedData()
+// }
+// seedData()
 
 
 const corsOptions = {
